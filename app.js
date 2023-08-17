f023e56e62e8a951457c01b9823b00cfcb3a0df6
Jesús Gándara Martínez <jesusalxen@gmail.com>
2023-08-17 23:00:35 +0200

    Change dotenv config

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
-import "dotenv/config";
+import { config } from "dotenv";
+config();
 
 import POIsRouter from "./routes/pois.js";
 import { corsMiddleware } from "./middlewares/cors.js";
