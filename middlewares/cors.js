4bb5edd4be7243f3d0399055c82b9d48c557f79b
Jesús Gándara Martínez <jesusalxen@gmail.com>
2023-08-20 16:10:36 +0200

    fix cors middleware

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,14 +5,14 @@ const ACCEPTED_OPTIONS = ["http://localhost:3000", "https://pois.vercel.app"];
 export const corsMiddleware = ({ acceptedOptions = ACCEPTED_OPTIONS } = {}) =>
   cors({
     origin: (origin, callback) => {
-      console.log({ origin, ACCEPTED_OPTIONS });
-      if (acceptedOptions.indexOf(origin)) {
+      if (acceptedOptions.indexOf(origin) > -1) {
         return callback(null, true);
       }
+
       if (!origin) {
         return callback(null, true);
       }
-      console.log("Not allowed by CORS");
+
       return callback(new Error("Not allowed by CORS"));
     },
   });
