a5b6ad25b10d831fa335da6455e460962953877f
Jesús Gándara Martínez <jesusalxen@gmail.com>
2023-08-20 16:07:17 +0200

    add more logging

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,8 +5,8 @@ const ACCEPTED_OPTIONS = ["http://localhost:3000", "https://pois.vercel.app"];
 export const corsMiddleware = ({ acceptedOptions = ACCEPTED_OPTIONS } = {}) =>
   cors({
     origin: (origin, callback) => {
+      console.log({ origin, ACCEPTED_OPTIONS });
       if (acceptedOptions.indexOf(origin)) {
-        console.log({ origin });
         return callback(null, true);
       }
       if (!origin) {
